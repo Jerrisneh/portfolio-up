4d3844494bfd2539402f0cfeecededa4e0f86d1f
Jeremiah <jerrisneh@gmail.com>
2025-02-11 20:24:42 +0100

    updated js

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
+
+const menuToggle = document.querySelector(".tuggle1");
+const navLinks = document.getElementById("nav-link");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+});
