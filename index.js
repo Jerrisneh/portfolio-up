98cd8ff56a4bd519e440932d3263fecd1f91c177
Jeremiah <jerrisneh@gmail.com>
2025-06-18 13:04:00 +0100

    update: changed img and links

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const menuBtn = document.querySelector(".menu-btn");
-    const navUl = document.querySelector(".nav-ul");
-    
-    menuBtn.addEventListener("click", function () {
-        navUl.classList.toggle("active");
-    });
+  // this toggles the menu active and inactive(open/close) when clicking the hamburger-icon
+  const menuBtn = document.querySelector(".menu-btn");
+  const navUl = document.querySelector(".nav-ul");
 
-    // Close the menu when clicking outside
-    document.addEventListener("click", function (event) {
-        if (!menuBtn.contains(event.target) && !navUl.contains(event.target)) {
-            navUl.classList.remove("active");
-        }
-    });
+  menuBtn.addEventListener("click", function () {
+    navUl.classList.toggle("active");
+  });
 
-    // Smooth scrolling for navigation links
-    const navLinks = document.querySelectorAll(".nav-ul a");
-    
-    navLinks.forEach(link => {
-        link.addEventListener("click", function (event) {
-            event.preventDefault();
-            const targetId = this.getAttribute("href").substring(1);
-            const targetSection = document.getElementById(targetId);
-            
-            if (targetSection) {
-                window.scrollTo({
-                    top: targetSection.offsetTop - 50,
-                    behavior: "smooth"
-                });
-                navUl.classList.remove("active");
-            }
+  // Close the menu when clicking outside
+  document.addEventListener("click", function (event) {
+    if (!menuBtn.contains(event.target) && !navUl.contains(event.target)) {
+      navUl.classList.remove("active");
+    }
+  });
+
+  // Smooth scrolling for navigation links
+  const navLinks = document.querySelectorAll(".nav-ul a");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      const targetId = this.getAttribute("href").substring(1);
+      const targetSection = document.getElementById(targetId);
+
+      if (targetSection) {
+        window.scrollTo({
+          top: targetSection.offsetTop - 50,
+          behavior: "smooth"
         });
+        navUl.classList.remove("active");
+      }
     });
+  });
 });
